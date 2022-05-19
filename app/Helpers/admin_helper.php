b0e2896f8e5355099d3df51dc8cdccfc0fbc370f
Dev-IT <martonif1107@gmail.com>
2022-05-19 17:00:51 +0700

    First Commit

---
+++ app/Helpers/admin_helper.php
@@ -0,0 +1,18 @@
+<?php
+function format_rupiah($angka){
+  $rupiah=number_format($angka,0,',','.');
+  return $rupiah;
+}
+
+function getMenu($user) {
+  // $user = $_SESSION['users'];
+  $db = db_connect();
+
+  return $result = $db->query("select * from module")->getResult();
+}
+
+function getSubmenu($moduleid) {
+  $db = db_connect();
+  return $db->query("select * from menu where moduleid = {$moduleid} order by urutan asc")->getResult();
+}
+?>
